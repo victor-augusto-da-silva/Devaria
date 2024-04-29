0ac5fcc6fcd6a5a9ea9291a9f2a3d6d9d71ffe98
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-04-29 20:46:29 -0300

    API que retorna a data

--- my-next-app/src/pages/api/data.ts
+++ my-next-app/src/pages/api/data.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest,NextApiResponse } from "next";
-
+import moment from "moment";
 
 export default (req: NextApiRequest, res: NextApiResponse)=>{
     
 
-    res.status(200).json({ data: new Date()});
+    res.status(200).json({ data: moment().format('DD/MM/YYYY HH:MM:SS')});
 }

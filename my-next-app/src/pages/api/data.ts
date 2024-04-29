09587fcc634d7f5693bc298b16e2d697db0bf5cf
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-04-29 17:45:28 -0300

    .

---
+++ my-next-app/src/pages/api/data.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest,NextApiResponse } from "next";
+
+
+export default (req: NextApiRequest, res: NextApiResponse)=>{
+    
+
+    res.status(200).json({ data: new Date()});
+}
--- my-next-app/src/pages/api/hello.ts
+++ my-next-app/src/pages/api/hello.ts
@@ -9,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  console.log(req.method);
+  res.status(200).json({ name: "Victor Silva" });
 }
